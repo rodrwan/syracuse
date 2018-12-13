ae352c3553dd0dce8e9124992d334335657d4efc
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2018-12-12 23:23:28 -0300

    add migrations

---
+++ postgres/migrations/20181212231831_base.sql
@@ -0,0 +1,25 @@
+-- base: provides basic functions and extensions used by sigiriya database.
+
+-- +goose Up
+-- +goose StatementBegin
+create function update_updated_at_column()
+returns trigger as $$
+  begin
+      new.updated_at = now();
+      return new;
+  end;
+$$ language plpgsql;
+
+create extension citext;
+create extension pgcrypto;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+drop function update_updated_at_column();
+drop extension citext;
+drop extension pgcrypto;
+-- +goose StatementEnd
+
+
+
---
+++ postgres/migrations/20181212231843_users.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+create table users (
+	id              uuid primary key default gen_random_uuid(),
+	email           text not null unique,
+    fullname        text not null,
+
+	created_at      timestamptz default now(),
+	updated_at      timestamptz default now(),
+	deleted_at      timestamptz
+);
+
+create trigger update_users_updated_at
+before update on users for each row execute procedure update_updated_at_column();
+-- +goose StatementEnd
+
+-- +goose Down
+drop table users cascade;
+
