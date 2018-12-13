8fb6e9510765bb192f52a872a80f57c161be278b
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2018-12-12 23:29:58 -0300

    add db tags

--- syracuse.go
+++ syracuse.go
@@ -1,10 +1,16 @@
 package syracuse
 
+import "time"
+
 // Citizen ...
 type Citizen struct {
-	ID       string
-	Email    string
-	Fullname string
+	ID       string `json:"id" db:"id"`
+	Email    string `json:"email" db:"email"`
+	Fullname string `json:"fullname" db:"fullname"`
+
+	CreatedAt time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at" db:"updated_at"`
+	DeletedAt *time.Time `json:"-" db:"deleted_at"`
 }
 
 // Citizens ...
